ce11153bba4644c0eade79fc6919427341848789
蒋政 <2020010880@secoder.net>
2022-12-25 14:40:48 +0800

    modify the mutilband blending

--- fusion.py
+++ fusion.py
@@ -40,40 +40,37 @@ class Fusion:
 
 	# 金字塔融合
 	def Multiband(self, result, imageB):
-		# 计算重叠区域边界
-		left_top, left_bottom = imageB.shape[1], imageB.shape[1]
-		for j in range(result.shape[1]):
-			if result[0][j][0] != 0 or result[0][j][1] != 0 or result[0][j][2] != 0:
-				left_top = j
-				break
-		for j in range(result.shape[1]):
-			if result[imageB.shape[0] - 1][j][0] != 0 or result[imageB.shape[0] - 1][j][1] != 0 or result[imageB.shape[0] - 1][j][2] != 0:
-				left_bottom = j
-				break
-		start = left_top if left_top < left_bottom else left_bottom
-
-		imageA = result[0 : imageB.shape[0], start : imageB.shape[1]]
-		result[0 : imageB.shape[0], 0 : start] = imageB[0 : imageB.shape[0], 0 : start]
-		imageB = imageB[0 : imageB.shape[0], start : imageB.shape[1]]
-
+		source = np.zeros(result.shape, dtype = result.dtype)
+		source[0 : imageB.shape[0], 0 : imageB.shape[1]] = imageB
 		num_levels = 6
-		mask = np.ones(imageA.shape, dtype = imageA.dtype)
-		mask[imageA == 0] = 0
 
+		mask = np.ones(result.shape, dtype = np.float32)
+		mask[result == 0] = 0
+		for i in range(imageB.shape[0]):
+			left = imageB.shape[1]
+			for j in range(imageB.shape[1]):
+				if result[i][j][0] != 0 or result[i][j][1] != 0 or result[i][j][2] != 0:
+					left = j
+					break
+			if left != imageB.shape[1]:
+				width = imageB.shape[1] - left
+				for j in range(left, imageB.shape[1]):
+					mask[i][j] = (j - left) / width
+		
 		# 生成高斯金字塔
-		GA = imageA.copy()
-		GB = imageB.copy()
+		GA = result.copy()
+		GB = source.copy()
 		GM = mask.copy()
-		gpA = [GA]
-		gpB = [GB]
-		gpM = [GM]
+		gpA = [np.float32(GA)]
+		gpB = [np.float32(GB)]
+		gpM = [np.float32(GM)]
 		for i in range(num_levels):
 			GA = cv.pyrDown(GA)
 			GB = cv.pyrDown(GB)
 			GM = cv.pyrDown(GM)
-			gpA.append(GA)
-			gpB.append(GB)
-			gpM.append(GM)
+			gpA.append(np.float32(GA))
+			gpB.append(np.float32(GB))
+			gpM.append(np.float32(GM))
 
 		# 生成拉普拉斯金字塔
 		lpA = [gpA[num_levels - 1]]
@@ -82,8 +79,8 @@ class Fusion:
 
 		for i in range(num_levels - 1, 0, -1):
 			size = (gpA[i - 1].shape[1], gpA[i - 1].shape[0])
-			LA = cv.subtract(gpA[i - 1], cv.pyrUp(gpA[i], dstsize = size))
-			LB = cv.subtract(gpB[i - 1], cv.pyrUp(gpB[i], dstsize = size))
+			LA = np.subtract(gpA[i - 1], cv.pyrUp(gpA[i], dstsize = size))
+			LB = np.subtract(gpB[i - 1], cv.pyrUp(gpB[i], dstsize = size))
 			lpA.append(LA)
 			lpB.append(LB)
 			gpMr.append(gpM[i - 1])
@@ -91,8 +88,7 @@ class Fusion:
 		# 生成融合金字塔
 		lp = []
 		for la, lb, gm in zip(lpA, lpB, gpMr):
-			# ls = la * gm + lb * (1 - gm)
-			ls = cv.addWeighted(la, 0.5, lb, 0.5, 0.0)
+			ls = la * gm + lb * (1.0 - gm)
 			lp.append(ls)
 
 		# 重建
@@ -100,13 +96,13 @@ class Fusion:
 		for i in range(1, num_levels):
 			size = (lp[i].shape[1], lp[i].shape[0])
 			ls_ = cv.pyrUp(ls_, dstsize = size)
-			ls_ = cv.add(ls_, lp[i])
+			ls_ = cv.add(ls_, np.float32(lp[i]))
 			ls_[ls_ > 255] = 255
 			ls_[ls_ < 0] = 0
 
+		ls_ = ls_.astype(np.uint8)
 		# 融合结果		
-		result[0 : imageB.shape[0], start : start + ls_.shape[1]] = ls_
-		return result
+		return ls_
 		
 
 
