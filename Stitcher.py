d374d7ffe1f022948ff7d310a864a6d62c33c3ec
于沛楠 <2020010877@secoder.net>
2023-01-07 22:38:48 +0800

    cut bad part

--- Stitcher.py
+++ Stitcher.py
@@ -13,24 +13,43 @@ class Stitcher:
         print("==================Stitching begin==================")
         n = len(images)
         
-        # find the keypoints and descriptors with SIFT
+        # A B C       0 -> 1 into 1 | -1 -> -2 into -2 | final: 3 // 2 = 1
+        # A B C D     0 -> 1 into 1 | -1 -> -2 into -2 | 1 -> 2 into 2 | final: 4 // 2 = 2   
+        # A B C D E   0 -> 1 into 1 | -1 -> -2 into -2 | 1 -> 2 into 2 | -2 -> -3 into -3 | final: 5 // 2 = 2
+        # A B C D E F 0 -> 1 into 1 | -1 -> -2 into -2 | 1 -> 2 into 2 | -2 -> -3 into -3 | 2 -> 3 into 3 | final: 6 // 2 = 3
+
+        i = 0
+        cnt = 0
         reverse = True
-        result = images[0]
-        for i in range(1, n):
-            reverse = not reverse
-            imageA = result
-            imageB = images[i]
+        imageA = None
+        imageB = None
+        while cnt < n - 1:
+            if reverse: # imageA -> imageB
+                imageA = images[i]
+                imageB = images[i + 1]
+                reverse = False
+                print("imageA: {} -> imageB: {}".format(i, i + 1))
+            else: # imageB -> imageA
+                imageA = images[-i - 2]
+                imageB = images[-i - 1]
+                reverse = True
+                print("imageB: {} -> imageA: {}".format(-i - 1, -i - 2))
+
+            # reverse == True: imageB -> imageA (left + H[right])
+            # reverse == False: imageA -> imageB (H[left] + right)
+
+            # find the keypoints and descriptors with SIFT
             kpA, desA = self.sift(imageA)
             kpB, desB = self.sift(imageB)
 
             # match features between the two images
             matcher = Matcher()
-            if reverse:
+            if reverse: 
                 matches = matcher.knnMatch(desB, desA, ratio)
-            else:
+            else: 
                 matches = matcher.knnMatch(desA, desB, ratio)
 
-            # compute the homography between the two sets of matched points
+            # # compute the homography between the two sets of matched points
             if reverse:
                 (H, status) = self.findHomography(kpB, kpA, matches, reprojThresh)
             else:
@@ -41,9 +60,15 @@ class Stitcher:
             
             # apply a perspective transform to stitch the images together
             result = self.fusion((imageA, imageB), H, fusionMethod, reverse=reverse, showAny=showAny)
+            if reverse:
+                images[-i - 2] = result
+                i += 1
+            else:
+                images[i + 1] = result
+            cnt += 1
 
         print("==================Stitching end==================")
-        return result
+        return images[n // 2]
 
     def sift(self, image):
         # from opencv documentation
@@ -111,26 +136,62 @@ class Stitcher:
                                 result[i][j] = img1[i-t[1]][j-t[0]]
                     # result[t[1]:h1+t[1],t[0]:w1+t[0]] = img1
                 else:
-                    result[t[1]:h2+t[1],t[0]:w2+t[0]] = img
+                    for i in range(t[1], h2+t[1]):
+                        for j in range(t[0], w2+t[0]):
+                            if result[i][j][0] == 0 or result[i][j][1] == 0 or result[i][j][2] == 0:
+                                result[i][j] = img2[i-t[1]][j-t[0]]
             return result
 
         # apply a perspective transform to stitch the images together
-        # result = cv.warpPerspective(imageB, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0] + imageB.shape[0]))
-        result = warpTwoImages(imageA, imageB, H, fusionMethod, reverse=reverse)
+        
+        # result = warpTwoImages(imageA, imageB, H, fusionMethod, reverse=reverse)
 
-        # if showAny:
-        #     cv_show('result A', result)
+        result = None
+        img = None
+        if reverse:
+            result = cv.warpPerspective(imageB, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
+            img = imageA
+        else:
+            result = cv.warpPerspective(imageA, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
+            # result = np.concatenate([np.zeros((imageA.shape[0], imageA.shape[1], 3)), result], axis=1)
+            img = imageB
+        # cv_show('result_tmp', result)
+        fusion = Fusion()
+        if fusionMethod == "poisson":
+            result = fusion.poisson(result, img)
+        elif fusionMethod == "weight": 
+            result = fusion.weigh_fussion(result, img)
+        elif fusionMethod == "multiband":
+            result = fusion.Multiband(result, img)
+        else:
+            if reverse:
+                for i in range(0, img.shape[0]):
+                    for j in range(0, img.shape[1]):
+                        if result[i][j][0] == 0 or result[i][j][1] == 0 or result[i][j][2] == 0:
+                            result[i][j] = img[i][j]
+            else:
+                for i in range(0, img.shape[0]):
+                    for j in range(0, img.shape[1]):
+                        if result[i][j][0] == 0 or result[i][j][1] == 0 or result[i][j][2] == 0:
+                            result[i][j] = img[i][j]
+            # result[0:img.shape[0], 0:img.shape[1]] = img
 
-        # img = imageA
-        # fusion = Fusion()
-        # if fusionMethod == "poisson":
-        #     result = fusion.poisson(result, img)
-        # elif fusionMethod == "weight": 
-        #     result = fusion.weigh_fussion(result, img)
-        # elif fusionMethod == "multiband":
-        #     result = fusion.Multiband(result, img)
-        # else:
-        #     result[0:img.shape[0], 0:img.shape[1]] = img
+        # cv_show('result_tmp', result)
+        cnt = 0
+        cut = 0
+        flag = False
+        for j in range(0, result.shape[1]): # 从左到右
+            cnt = 0
+            for i in range(0, result.shape[0]): # 从上到下
+                if result[i][j][0] == 0 or result[i][j][1] == 0 or result[i][j][2] == 0:
+                    cnt += 1
+            cut = j
+            if cnt >= result.shape[0] * 0.8 and flag == True:
+                break 
+            else:
+                flag = True
+        result = result[:, :cut, :]
+        # cv_show('result', result)
         return result
 
 
